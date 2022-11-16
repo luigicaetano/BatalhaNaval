eb267b377682fba466fb49b5da90e88871c7d639
Luigi Caetano <luigicaetano@gmail.com>
2022-11-16 09:35:30 -0300

    v1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from pygame.locals import *
 
 
 def main():
@@ -11,20 +11,28 @@ def main():
     screen = pygame.display.set_mode((x, y))
     # logo = pygame.image.load("logo32x32.png")
     # pygame.display.set_icon(logo)
-    pygame.display.set_caption("minimal program")
-    bg = pygame.image.load('images/fundo.jpg')
+    pygame.display.set_caption("Batalha Naval")
+    bg = pygame.image.load('images/fundo.jpg').convert()
     bg = pygame.transform.scale(bg, (x, y))
-
-
+    mar = pygame.image.load('images/mar.png').convert()
+    mar = pygame.transform.scale(mar, (46, 46))
+    screen.blit(bg, (0, 0))
     running = True
 
     while running:
+        primeiro_quadrado = Rect(70, 70, 46, 46)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        screen.blit(bg, (0, 0))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = event.pos
+                if primeiro_quadrado.collidepoint(mouse_pos):
+                    screen.blit(mar, (70,70))
+
         pygame.display.update()
+
 
 if __name__ == "__main__":
     main()
