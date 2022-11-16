4f9e04401b7b8353397c5dad0b875050d137f766
Luigi Caetano <luigicaetano@gmail.com>
2022-11-16 12:45:17 -0300

    v1

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from pygame.locals import *
-
+import json
 
 def main():
+
+    f = open('coordenadas.json')
+    data = json.load(f)
+
     pygame.init()
 
     x = 600
@@ -12,15 +16,22 @@ def main():
     # logo = pygame.image.load("logo32x32.png")
     # pygame.display.set_icon(logo)
     pygame.display.set_caption("Batalha Naval")
+
     bg = pygame.image.load('images/fundo.jpg').convert()
     bg = pygame.transform.scale(bg, (x, y))
+
     mar = pygame.image.load('images/mar.png').convert()
     mar = pygame.transform.scale(mar, (46, 46))
+
+    acerto = pygame.image.load('images/acerto.png').convert()
+    acerto = pygame.transform.scale(acerto, (46, 46))
+
     screen.blit(bg, (0, 0))
     running = True
 
     while running:
         primeiro_quadrado = Rect(70, 70, 46, 46)
+        segundo_quadrado = Rect(116, 70, 46, 46)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -29,7 +40,9 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
                 if primeiro_quadrado.collidepoint(mouse_pos):
-                    screen.blit(mar, (70,70))
+                    screen.blit(mar, (70, 70))
+                if segundo_quadrado.collidepoint(mouse_pos):
+                    screen.blit(acerto, (116, 70))
 
         pygame.display.update()
 
