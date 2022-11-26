e36862f52faacdef6cfa813d7b95d21b3f98a39f
Luigi Caetano <luigicaetano@gmail.com>
2022-11-26 07:28:20 -0300

    Criada forma de verificar qual o tamanho do navio acertado, e retirado os prints do código.

--- main.py
+++ main.py
@@ -56,7 +56,6 @@ def coordenadas_alteradas():
             coordenadas5_2.append(u)
 
 
-
 def regras_navios(orientacao, tamanho):
     coord = ""
     if orientacao == 1 and tamanho == 1:
@@ -86,26 +85,14 @@ def evita_colisao(tam, orient, x_inicial, y_inicial):
     tam -= 1
     while tam >= 0:
         if orient == 1:
-            #print(f'Tamanho: {tam}, x a remover: {(tam * 46) + x_inicial}, y a remover: {y_inicial}')
-            #print(f'Coordenadas: {coordenadas}')
             removidos.append([(tam * 46) + x_inicial, y_inicial])
             coordenadas.remove([(tam * 46) + x_inicial, y_inicial])
         else:
-            #print(f'Tamanho: {tam}, x a remover: {x_inicial}, y a remover: {(tam * 46) + y_inicial}')
-            #print(f'Coordenadas: {coordenadas}')
             removidos.append([x_inicial, (tam * 46) + y_inicial])
             coordenadas.remove([x_inicial, (tam * 46) + y_inicial])
         tam -= 1
     coordenadas_alteradas()
-    #print(coordenadas2_1)
-    #print(coordenadas2_2)
-    #print(coordenadas3_1)
-    #print(coordenadas3_2)
-    #print(coordenadas4_1)
-    #print(coordenadas4_2)
-    #print(coordenadas5_1)
-    #print(coordenadas5_2)
-    #print(f' Removidos: {removidos}')
+
 
 def confere_colisao(tam, orient):
     conferindo = True
@@ -119,11 +106,9 @@ def confere_colisao(tam, orient):
         else:
             navio_atual.append([navio_coord[0], (tam1 * 46) + navio_coord[1]])
         tam1 -= 1
-    #print(f'Navio atual {navio_atual}')
     while conferindo:
         tam2 = tam - 1
         if any(x in navio_atual for x in removidos):
-            #print(f'Errado: {navio_atual}')
             nova_coord = regras_navios(orient, tam)
             navio_atual = []
             while tam2 >= 0:
@@ -133,14 +118,13 @@ def confere_colisao(tam, orient):
                     navio_atual.append([nova_coord[0], (tam2 * 46) + nova_coord[1]])
                 tam2 -= 1
         else:
-            #print(f'Certo: {navio_atual}')
             navios_coord.append(navio_atual)
             conferindo = False
     return nova_coord
 
+
 def criar_navios(tamanho):
     orientacao = random.choice([1, 2])
-    #coord = regras_navios(orientacao, tamanho)
     coord = confere_colisao(tamanho, orientacao)
 
     x_inicial = coord[0]
@@ -153,7 +137,36 @@ def criar_navios(tamanho):
     evita_colisao(tamanho, orientacao, x_inicial, y_inicial)
 
 
+def escolher_imagem(tamanho, orientacao):
+    imagem = ""
+    if orientacao == 2 and tamanho == 2:
+        imagem = barco2_2
+    elif orientacao == 1 and tamanho == 2:
+        imagem = barco2_1
+    elif orientacao == 1 and tamanho == 3:
+        imagem = barco3_1
+    elif orientacao == 2 and tamanho == 3:
+        imagem = barco3_2
+    elif orientacao == 1 and tamanho == 4:
+        imagem = barco4_1
+    elif orientacao == 2 and tamanho == 4:
+        imagem = barco4_2
+    elif orientacao == 1 and tamanho == 5:
+        imagem = barco5_1
+    elif orientacao == 2 and tamanho == 5:
+        imagem = barco5_2
+    return imagem
+
+
 def main():
+    global barco2_1
+    global barco2_2
+    global barco3_1
+    global barco3_2
+    global barco4_1
+    global barco4_2
+    global barco5_1
+    global barco5_2
     pygame.init()
 
     x = 600
@@ -176,13 +189,35 @@ def main():
     acerto = pygame.image.load('images/acerto.png').convert()
     acerto = pygame.transform.scale(acerto, (46, 46))
 
+    barco2_1 = pygame.image.load('images/barco2-1.png').convert()
+    barco2_1 = pygame.transform.scale(barco2_1, (92, 46))
+
+    barco2_2 = pygame.image.load('images/barco2-2.png').convert()
+    barco2_2 = pygame.transform.scale(barco2_2, (46, 92))
+
+    barco3_1 = pygame.image.load('images/barco3-1.png').convert()
+    barco3_1 = pygame.transform.scale(barco3_1, (138, 46))
+
+    barco3_2 = pygame.image.load('images/barco3-2.png').convert()
+    barco3_2 = pygame.transform.scale(barco3_2, (46, 138))
+
+    barco4_1 = pygame.image.load('images/barco4-1.png').convert()
+    barco4_1 = pygame.transform.scale(barco4_1, (184, 46))
+
+    barco4_2 = pygame.image.load('images/barco4-2.png').convert()
+    barco4_2 = pygame.transform.scale(barco4_2, (46, 184))
+
+    barco5_1 = pygame.image.load('images/barco5-1.png').convert()
+    barco5_1 = pygame.transform.scale(barco5_1, (230, 46))
+
+    barco5_2 = pygame.image.load('images/barco5-2.png').convert()
+    barco5_2 = pygame.transform.scale(barco5_2, (46, 230))
+
     pontos = 30
     font = pygame.font.Font('freesansbold.ttf', 20)
     text = font.render("Erros restantes: " + str(pontos), True, (250, 250, 250), (78, 187, 174))
     textRect = text.get_rect()
     textRect.center = (150, 570)
-
-    acerto_point = 0
 
     navios_acertados = 10
 
@@ -190,8 +225,6 @@ def main():
     naviosRect = text_navios.get_rect()
     naviosRect.center = (450, 570)
 
-    #criar_navios(1)
-    #criar_navios(1)
     criar_navios(2)
     criar_navios(2)
     criar_navios(2)
@@ -210,8 +243,6 @@ def main():
     screen.blit(bg, (0, 0))
     running = True
 
-    #print(navios)
-
     while running:
 
         for event in pygame.event.get():
@@ -220,18 +251,16 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mouse_pos = event.pos
-                #print(mouse_pos[0])
                 for p in mares:
                     if p.collidepoint(mouse_pos):
                         screen.blit(mar, (p[0], p[1]))
                         pontos = pontos - 1
                         text = font.render(f'Erros restantes: {pontos}', True, (250, 250, 250), (78, 187, 174))
-                        if(pontos <= 0):
+                        if pontos <= 0:
                             text = font.render(f'GAME OVER!!', True, (250, 250, 250), (78, 187, 174))
                 for p in navios:
                     if p.collidepoint(mouse_pos):
                         for i in range(len(options) - 1):
-                            #print(mouse_pos[0], options[i + 1])
                             if mouse_pos[0] < options[i + 1]:
                                 x_pos = options[i]
                                 break
@@ -245,22 +274,40 @@ def main():
                                 y_pos = 484
                         navio_acertado.append([x_pos, y_pos])
                         screen.blit(acerto, (x_pos, y_pos))
-                        acerto_point = acerto_point + 1
-                        if(acerto_point >= 30):
-                            text = font.render(f'YOU WIN!!', True, (250, 250, 250), (78, 187, 174))
                 counter = 0
+                x_navio = 1000
+                y_navio = 1000
                 for i in navios_coord:
                     for k in i:
                         if k in navio_acertado:
                             counter += 1
-
                     if counter == len(i):
+                        for k in i:
+                            if k[0] < x_navio:
+                                x_navio = k[0]
+                            if k[1] < y_navio:
+                                y_navio = k[1]
+                        for p in navios:
+                            if p[0] == x_navio and p[1] == y_navio:
+                                if p[2] > p[3]:
+                                    orientacao_navio = 1
+                                    tamanho_navio = int(p[2]/p[3])
+                                else:
+                                    orientacao_navio = 2
+                                    tamanho_navio = int(p[3]/p[2])
+                                p.w = 0
+                                p.h = 0
+                                barco = escolher_imagem(tamanho_navio, orientacao_navio)
+                                screen.blit(barco, (x_navio, y_navio))
                         navios_coord.remove(i)
                         counter = 0
                         navios_acertados = navios_acertados - 1
                         text_navios = font.render("Navios restantes: " + str(navios_acertados), True,
                                                   (250, 250, 250),
                                                   (78, 187, 174))
+                        if navios_acertados == 0:
+                            text = font.render(f'YOU WIN!!', True, (250, 250, 250), (78, 187, 174))
+                        break
                     else:
                         counter = 0
 
